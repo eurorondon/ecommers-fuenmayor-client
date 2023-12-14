250502ee6e8b7baf6e49da9149bf4908c9d783ce
eurorondon03 <eurorondon03@gmail.com>
2023-12-14 09:24:38 -0400

    consultando los datos con graphql y reactquery

--- src/components/homeComponents/ShopSection/GridProductList.js
+++ src/components/homeComponents/ShopSection/GridProductList.js
@@ -1,24 +1,83 @@
+import React from "react";
 import Product from "./ProductGrid";
 import { Link } from "react-router-dom";
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { isError, useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { getProducts } from "../../../api/productsApi";
 import Loading from "../../Loading";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-const GridProductList = () => {
-  const { data, isLoading, error, hasNextPage, fetchNextPage } =
-    useInfiniteQuery(
-      ["infinity-products"],
-      ({ pageParam = 0 }) =>
-        getProducts(`/api/products?pageNumber=${pageParam}`),
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/api";
+import { listProductListFuenmayors } from "../../../graphql/queries";
+import amplifyconfig from "../../../amplifyconfiguration.json";
 
-      {
-        getNextPageParam: (lastPage) => {
-          if (lastPage.page === lastPage.pages) return false;
-          return lastPage.page + 1;
-        },
+Amplify.configure(amplifyconfig);
+const client = generateClient();
+
+const GridProductList = () => {
+  const [products, setProducts] = React.useState([]);
+
+  // React.useEffect(() => {
+  //   fetchProducts();
+  // }, []);
+
+  const { data, isLoading, isError } = useQuery(
+    ["listProductListFuenmayors"],
+    async () => {
+      try {
+        const productsData = await client.graphql({
+          query: listProductListFuenmayors,
+          variables: {
+            limit: 20,
+            filter: { nombre: { contains: "" } },
+          },
+        });
+
+        return productsData.data.listProductListFuenmayors.items;
+      } catch (err) {
+        console.error("Error fetching todos", err);
+        throw err;
       }
-    );
+    },
+    {
+      onSuccess: (data) => {
+        setProducts(data);
+      },
+    }
+  );
+
+  console.log(data, isError);
+
+  // async function fetchProducts() {
+  //   try {
+  //     const productsData = await client.graphql({
+  //       query: listProductListFuenmayors,
+  //       variables: {
+  //         limit: 20,
+  //         filter: { nombre: { contains: "" } },
+  //       },
+  //     });
+  //     const products = productsData;
+
+  //     setProducts(products.data.listProductListFuenmayors.items);
+  //   } catch (err) {
+  //     console.log("error fetching todos", err);
+  //   }
+  // }
+  // console.log(products.data?.listProductListFuenmayors?.items);
+  // const { data, isLoading, error, hasNextPage, fetchNextPage } =
+  //   useInfiniteQuery(
+  //     ["infinity-products"],
+  //     ({ pageParam = 0 }) =>
+  //       getProducts(`/api/products?pageNumber=${pageParam}`),
+
+  //     {
+  //       getNextPageParam: (lastPage) => {
+  //         if (lastPage.page === lastPage.pages) return false;
+  //         return lastPage.page + 1;
+  //       },
+  //     }
+  //   );
 
   // const productList =
   //   data?.pages.reduce(
@@ -26,41 +85,55 @@ const GridProductList = () => {
   //     []
   //   ) ?? [];
 
-  const productList = data?.pages.flatMap((page) => page.products) ?? [];
+  // const productList = data?.pages.flatMap((page) => page.products) ?? [];
 
-  if (isLoading) return <Loading />;
-  else {
-    return (
-      <>
-        <h2>Todos los Articulos</h2>
-        <InfiniteScroll
-          dataLength={productList.length}
-          hasMore={hasNextPage}
-          next={() => fetchNextPage()}
-          loader={
-            <div className="mx-auto">
-              <Loading />
-            </div>
-          }
-        >
-          <div className=" grid mx-auto ">
-            {productList?.map((product) => (
-              <div key={product._id}>
-                <Link to={`/products/${product._id}`}>
-                  <Product
-                    url={product.photo[0].url}
-                    name={product.name}
-                    description={product.description}
-                    price={product.price}
-                  />
-                </Link>
-              </div>
-            ))}
-          </div>
-        </InfiniteScroll>
-      </>
-    );
-  }
+  // if (isLoading) return <Loading />;
+  // else {
+  return (
+    <div className=" grid mx-auto ">
+      {products?.map((product) => (
+        <div key={product._id}>
+          <Link to={`/products/${product._id}`}>
+            <Product
+              // url={product.photo[0].url}
+              name={product.nombre}
+              description={product.departamento}
+              price={product.price}
+            />
+          </Link>
+        </div>
+      ))}
+    </div>
+    // <>
+    //   <h2>Todos los Articulos</h2>
+    //   <InfiniteScroll
+    //     dataLength={productList.length}
+    //     hasMore={hasNextPage}
+    //     next={() => fetchNextPage()}
+    //     loader={
+    //       <div className="mx-auto">
+    //         <Loading />
+    //       </div>
+    //     }
+    //   >
+    //     <div className=" grid mx-auto ">
+    //       {products?.map((product) => (
+    //         <div key={product._id}>
+    //           <Link to={`/products/${product._id}`}>
+    //             <Product
+    //               // url={product.photo[0].url}
+    //               name={product.nombre}
+    //               description={product.description}
+    //               price={product.price}
+    //             />
+    //           </Link>
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </InfiniteScroll>
+    // </>
+  );
 };
+// };
 
 export default GridProductList;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,56 +2,25 @@ import React from "react";
 import ShopSections from "../components/homeComponents/ShopSection/ShopSection";
 import Header from "../components/Header";
 import Categorias from "../components/homeComponents/Categorias";
-import NewProducts from "../components/homeComponents/NewProducts/NewProducts";
-import Destacados from "../components/homeComponents/Destacados/Destacados";
-import Ofertas from "../components/homeComponents/Ofertas/Ofertas";
 import { useSelector } from "react-redux";
 import Portada from "../components/homeComponents/Portada";
 import Whatsapp from "../components/homeComponents/Whatsapp";
 
-import { Amplify } from "aws-amplify";
-import { generateClient } from "aws-amplify/api";
-import { listProductListFuenmayors } from "../graphql/queries";
-import amplifyconfig from "../amplifyconfiguration.json";
-Amplify.configure(amplifyconfig);
-const client = generateClient();
-
 // import GridProductList from "../components/homeComponents/GridProductList";
 
 const Home = () => {
-  const [products, setProducts] = React.useState([]);
   const { isLoading } = useSelector((state) => state.products);
 
-  React.useEffect(() => {
-    fetchProducts();
-  }, []);
-
-  async function fetchProducts() {
-    try {
-      const productsData = await client.graphql({
-        query: listProductListFuenmayors,
-        variables: {
-          // limit: 10,
-          filter: { nombre: { contains: "" } },
-        },
-      });
-      const products = productsData;
-
-      setProducts(products);
-    } catch (err) {
-      console.log("error fetching todos", err);
-    }
-  }
-  console.log(products.data?.listProductListFuenmayors?.items);
+  // console.log(products.data?.listProductListFuenmayors?.items);
 
   return (
     <div className="bg-neutral-100">
       <Header />
       <Portada />
       <Categorias />
-      <NewProducts />
+      {/* <NewProducts />
       <Ofertas />
-      <Destacados />
+      <Destacados /> */}
       {isLoading ? null : <></>}
       <ShopSections />
       <Whatsapp />
